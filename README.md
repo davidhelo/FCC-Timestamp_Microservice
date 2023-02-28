9b41a84c787cd499553945eafa9c16b1bbc6e95c
davidhelo <dohernandez93@gmail.com>
2023-02-28 19:39:58 +0000

    feat: Return timestamp microservice completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Timestamp Microservice
+Project by David
+This is the boilerplate code for the Timestamp Microservice project. 
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
--- index.js
+++ index.js
@@ -24,7 +24,31 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/", function (req, res) {
+  let date = new Date(Date.now());
+  res.json({"date": date.toUTCString()});
+});
 
+app.get("/api/:date", function (req, res) {
+  let date = undefined; // initialise variable 
+
+  // Verify if variable is a number or date string
+  if (!isNaN(req.params.date)){
+    date = new Date(parseInt(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
+  
+  if (date == "Invalid Date") {
+    res.json({ "error": "Invalid Date" });
+  } else {
+    console.log(date);
+    res.json({
+      "unix": Date.parse(date),
+      "utc": date.toUTCString()
+    });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
